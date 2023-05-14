6a5681cf67f5bd3bdd4ccc28e4a8b2241f8a5aca
adrazdova <alesiadrozdova@gmail.com>
2023-05-14 19:18:52 +0200

    First commit

---
+++ carinsurance/carinsurance/src/main/java/com/bootcamp/carinsurance/CarinsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.carinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarinsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarinsuranceApplication.class, args);
+	}
+
+}
---
+++ carinsurance/carinsurance/src/test/java/com/bootcamp/carinsurance/CarinsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.carinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
